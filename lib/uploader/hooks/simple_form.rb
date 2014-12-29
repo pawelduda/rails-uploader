b7cfa814db7e264ba080ebc5f51f7f891cc4a88a
tbprojects <info.tbprojects@gmail.com>
2014-12-29 13:00:22 +0100

    fix simple form deprecation

--- lib/uploader/hooks/simple_form.rb
+++ lib/uploader/hooks/simple_form.rb
@@ -1,7 +1,7 @@
 require "simple_form"
 
 class UploaderInput < ::SimpleForm::Inputs::Base
-  def input
+  def input(wrapper_options)
     @builder.uploader_field(attribute_name, input_html_options)
   end
 end
