b27c262a69dd268a6e24e9f65244fccf52452447
tbprojects <info.tbprojects@gmail.com>
2015-05-07 20:24:38 +0200

    fixed gem issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
-require "rspec/rails"
 require "database_cleaner"
 
 # Fixtures replacement with a straightforward definition syntax
